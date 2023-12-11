4ee9fc865ef97b9e00d85034e80cbcebd07f9a35
rustumsk <rustumarellano@gmail.com>
2023-12-11 10:25:37 +0800

    new file structure

---
+++ driver.java
@@ -0,0 +1,8 @@
+import OOP_Project.view.*;
+import OOP_Project.model.*;
+import OOP_Project.model.database.Data;
+public class driver{
+    public static void main(String[] args){
+        new WelcomePage();
+    }
+}
