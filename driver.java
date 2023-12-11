bfbd4f888b48d5fbf37eba3ecac1f2b8393754ab
rustumsk <rustumarellano@gmail.com>
2023-12-11 21:25:55 +0800

    ...

--- driver.java
+++ driver.java
@@ -3,6 +3,6 @@ import OOP_Project.model.*;
 import OOP_Project.model.database.Data;
 public class driver{
     public static void main(String[] args){
-        new SearchPage();
+        new RegistrationPage();
     }
 }
